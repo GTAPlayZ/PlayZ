112005cb4d03b33dd5ae1579f0a2750207998a5d
PlayZ <92698625+GTAPlayZ@users.noreply.github.com>
2021-12-10 21:16:27 -0300

    Revert "add servidor de backend;"
    This reverts commit ce60cda516dc2af89c80143564ce4b1d07847db7.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,4 @@
 var paginaAtual = "";
-const hostServer = "http://127.0.0.1:3000";
 
 // apos fazer a renderização da tela executara o script
 $(document).ready(function() {
@@ -43,29 +42,5 @@ function loadPage(page) {
     $("#page-container").load(`./views/${page}/index.html`, function(responseTxt, statusTxt, xhr) {
         if(statusTxt == "error")
           $(this).html(xhr.statusText);
-    });
-}
-
-/* enviar para o servidor */
-/**
- * 
- * @param {*} query 
- * @param {*} url 
- * @param {*} method 
- * @param {*} callback 
- */
-function requestServer(query, url, method, callback) {
-    $.ajax({
-        type: method,
-        url: url,
-        data: query,
-        cache: false,
-        success: function (retornoServer) {
-            callback(null, retornoServer);
-        },
-        error: function (err) {
-            console.log(err);
-            callback(err);
-        }
     });
 }
